98aae21cdd154ae5f1eda5c9a70980c10f9115de
Divyanshi Sengar <sengardivya1503@gmail.com>
2026-03-23 02:50:22 +0530

    created blog and blogpost page

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,9 +1,40 @@
 import React from 'react'
+import Link from 'next/link'
 
 const Blog = () => {
   return (
     <div>
-      <h1>this is blog page</h1>
+      <div className="flex flex-col gap-5  mt-20 items-center justify-center">
+        {/* card section */}
+        {/* <h2 className="text-2xl font-semibold">Popular blog posts</h2> */}
+        {/* card */}
+        <div className="c">
+          <Link href="blogpost\Hello mitro !">
+            <h3 className="text-xl font-semibol cursor-pointer">How to learn Next.js in 2026.</h3>
+          </Link>
+          <p className="text-base text-zinc-600 dark:text-zinc-400">
+            I will teach you how to learn Next.js in 2026.
+          </p>
+        </div>
+
+        <div className="c">
+          <Link href="blogpost\Hello mitro !">
+            <h3 className="text-xl font-semibol cursor-pointer">How to learn Next.js in 2026.</h3>
+          </Link>
+          <p className="text-base text-zinc-600 dark:text-zinc-400">
+            I will teach you how to learn Next.js in 2026.
+          </p>
+        </div>
+
+        <div className="c">
+          <Link href="blogpost\Hello mitro !">
+            <h3 className="text-xl font-semibol cursor-pointer">How to learn Next.js in 2026.</h3>
+          </Link>
+          <p className="text-base text-zinc-600 dark:text-zinc-400">
+            I will teach you how to learn Next.js in 2026.
+          </p>
+        </div>
+      </div>
     </div>
   )
 }
--- app/blogpost/[id]/page.tsx
+++ app/blogpost/[id]/page.tsx
@@ -4,8 +4,10 @@ const Blogpost = async ({ params }: { params: { id: string } }) => {
     const { id } = await params;
      const decodedId = decodeURIComponent(id);
   return (
-    <div>
-      <h1>Blog Post: {decodedId}</h1>
+    <div className='mt-20 px-80 py-10'>
+      <h1 className='text-4xl pb-5'>Title of the page {decodedId}</h1>
+      <hr />
+      <p className='mt-8'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Necessitatibus natus dolorem quisquam numquam, impedit tempora, nemo, tenetur ex officiis accusantium illum odit voluptates soluta sint non quae voluptatem praesentium provident.</p>
     </div>
   )
 }

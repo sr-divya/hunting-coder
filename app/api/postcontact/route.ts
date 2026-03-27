f59bad3224a4e8b5b93be8722d0e699060eef18d
Divyanshi Sengar <sengardivya1503@gmail.com>
2026-03-28 01:16:02 +0530

    create API to save contact data

---
+++ app/api/postcontact/route.ts
@@ -0,0 +1,27 @@
+// app/api/example/route.js
+
+import { NextRequest, NextResponse } from "next/server";
+import * as fs from "fs";
+
+
+export async function POST(request: Request) {
+    try {
+
+        const body = await request.json();
+        console.log("Done");
+
+        let data=await fs.promises.readdir('contactdata');
+        // console.log(data);
+        fs.promises.writeFile(`contactdata/${data.length + 1}.json`,JSON.stringify(body));
+
+        return NextResponse.json({
+            message: "POST request received",
+            data: body,
+        }, { status: 200 });
+    } catch (error) {
+        return NextResponse.json({
+            message: "Error processing POST request",
+        }, { status: 500 });
+    }
+}
+

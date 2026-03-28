faafe57af14cef285ace67f5e27b09010db3c629
Divyanshi Sengar <sengardivya1503@gmail.com>
2026-03-28 18:20:07 +0530

    design about page

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -2,9 +2,18 @@ import React from 'react'
 
 const Page = () => {
   return (
-    <div className=''>
-      <h1>this is about page.</h1>
-      
+    <div className='px-90 flex-col py-10'>
+      <div className='flex items-center justify-center '>
+        <h1 className='text-3xl flex font-semibold mb-8 '>About Hunting Coder</h1>
+      </div>
+      <h2 className='text-2xl font-semibold mb-3'>Introduction</h2>
+      <p className='text-base'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore magni mollitia harum. Repellendus repudiandae minus harum, facilis recusandae velit assumenda unde dolore aut ad voluptates quidem consequuntur neque iusto quae debitis ipsum? Vero, eos! Atque perspiciatis minus dicta facere, in amet eum, ea quisquam pariatur illo delectus sapiente ipsum ut omnis. Accusamus, enim! Dolores, dignissimos ea reiciendis consectetur ipsam ab aliquam sequi porro aperiam autem aliquid odit reprehenderit repellat esse quidem voluptates fugit commodi itaque incidunt nostrum atque magnam! Ratione labore sint minus.</p>
+
+      <h2 className='text-2xl font-semibold my-5'>Our Mission</h2>
+      <p className='text-base'>At Hunting Coder, our mission is to provide high-quality coding tutorials and resources to help developers of all levels improve their skills and stay up-to-date with the latest technologies.</p>
+
+      <h2 className='text-2xl font-semibold my-5'>Services Offered</h2>
+      <p className='text-base'>We offer a wide range of services to help you advance your coding skills, including online courses, personalized mentoring, and hands-on projects.</p>
     </div>
   )
 }
